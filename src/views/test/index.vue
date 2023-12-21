c585af89eebb1d4b503f58c251c1618feeab804b
xuguang-web <2529073813@qq.com>
2023-12-21 23:58:06 +0800

    fix: 调用本地接口

---
+++ src/views/test/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div >
+      66
+  </div>
+</template>
+
+<script setup lang="ts" name="test">
+import axios from 'axios'
+axios.post('http://localhost:8000/login',{
+    name:"kobe",
+    password: "123456"
+}).then(res => {
+    console.log('%c [  ]-15', 'font-size:13px; background:pink; color:#bf2c9f;', res)
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
